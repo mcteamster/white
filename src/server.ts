81c97b5267fb0f8e40ce35b9bfffbfa9993304f8
tonz <mcteamster@gmail.com>
2025-02-05 00:09:58 +1100

    feat: add storage cache

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import { Server, Origins, FlatFile } from 'boardgame.io/server';
+import { StorageCache } from '@boardgame.io/storage-cache';
 import { BlankWhiteCards } from './Game';
 import { customAlphabet, nanoid } from 'nanoid';
 const roomCodeGen = customAlphabet('BCDFGHJKLMNPQRSTVWXYZ', 4);
 
+const db = new FlatFile({
+  dir: './db',
+  logging: false,
+  ttl: true,
+});
+const dbWithCaching = new StorageCache(db, { cacheSize: 200 });
+
 const server = Server({
   games: [BlankWhiteCards],
-  db: new FlatFile({
-    dir: './db',
-    logging: false,
-    ttl: true,
-  }),
+  db: dbWithCaching,
   origins: [Origins.LOCALHOST_IN_DEVELOPMENT, 'https://white.mcteamster.com'],
   uuid: roomCodeGen,
   generateCredentials: () => nanoid(),
