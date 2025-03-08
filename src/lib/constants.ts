09e75b3b5731358cdadb300366ab124683c9bae8
tonz <mcteamster@gmail.com>
2025-03-09 00:31:56 +1100

    feat: refresh standard daily

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -17,12 +17,14 @@ const refreshDeck = (deck: string) => {
     });
 
     if (deck == 'global') {
-      setTimeout(refreshDeck, 1000 * 60 * 10, 'global'); // Refresh only global every 10 minutes thereafter
+      setTimeout(refreshDeck, 1000 * 60 * 10, 'global'); // Refresh global every 10 minutes thereafter
+    } else if (deck == 'standard') {
+      setTimeout(refreshDeck, 1000 * 60 * 60 * 24, 'standard'); // Refresh standard daily
     }
   } catch (e) {
     console.error(e)
   }
 }
 
-setTimeout(refreshDeck, 1000 * 15, 'global'); // Refresh after first 15 seconds
-setTimeout(refreshDeck, 1000 * 30, 'standard'); // Refresh once first 30 seconds
+setTimeout(refreshDeck, 1000 * 10, 'global'); // 10 second delay
+setTimeout(refreshDeck, 1000 * 20, 'standard'); // 20 second delay
