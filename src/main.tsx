0c6aa4742d750bbc8c4b98655fbe9709c792a19a
tonz <mcteamster@gmail.com>
2025-03-26 20:36:43 +1100

    feat: use clarity package

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Clarity from '@microsoft/clarity';
+
+// Initialize Clarity only for official deployment
+if (import.meta.env.VITE_CLARITY_ID && import.meta.env.VITE_ORIGIN == 'https://white.mcteamster.com') {
+  Clarity.init(import.meta.env.VITE_CLARITY_ID);
+  console.debug('Clarity enabled');
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
