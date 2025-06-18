2c7b1ae365ce1e5fba0e9f615bb78180f38c540f
mcteamster <28884260+mcteamster@users.noreply.github.com>
2025-06-18 22:12:03 +1000

    chore: update virgo

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { GlobalBlankWhiteCardsClient, parsePathCode, getRegion, startingDeck, lo
 import { Rotate } from "./Components/Icons";
 import { useHotkeys, useWindowDimensions } from "./lib/hooks";
 import { Gallery } from "./Components/Gallery";
-import { Virgo2AWS } from 'virgo';
+import { Virgo2AWS } from '@mcteamster/virgo';
 
 // Landing Page
 const App = () => {
