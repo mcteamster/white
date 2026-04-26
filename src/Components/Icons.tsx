ba83c08d870f75cd0ceac6b6a18447bec1aa7f08
mcteamster <28884260+mcteamster@users.noreply.github.com>
2026-04-26 23:22:16 +1000

    Use square (Stop) icon for Solid mode

--- src/Components/Icons.tsx
+++ src/Components/Icons.tsx
@@ -1,7 +1,6 @@
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
-import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import GrainIcon from '@mui/icons-material/Grain';
 import BackHandOutlinedIcon from '@mui/icons-material/BackHandOutlined';
 import BookOutlinedIcon from '@mui/icons-material/BookOutlined';
@@ -145,7 +144,7 @@ export function Icon(props: { name: IconName }) {
     stop: <StopIcon></StopIcon>,
     show: <VisibilityIcon></VisibilityIcon>,
     hide: <VisibilityOffIcon></VisibilityOffIcon>,
-    solid: <FiberManualRecordIcon></FiberManualRecordIcon>,
+    solid: <StopIcon></StopIcon>,
     stipple: <GrainIcon></GrainIcon>,
     wand: <AutoFixHighIcon></AutoFixHighIcon>,
     weight: <LineWeightSharpIcon></LineWeightSharpIcon>,
